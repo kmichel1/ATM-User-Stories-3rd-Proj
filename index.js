b1914f71f667afc9700171e268cbee7b578ad3fe
kmichel1 <kmike411@yahoo.com>
2021-09-01 16:35:18 -0400

    Created user menu to interact with ATM file.

--- index.js
+++ index.js
@@ -4,3 +4,51 @@ let atmInfo = require('./atm')
 console.log(atmInfo.getBalance);
 
 
+function atmMenu(){
+    if(pinAccess){
+      
+    }
+  
+    if(!pinAccess){
+      alert("incorrect pin number");
+      return atmAccess(pinAccess);
+    }
+  
+    let displayAtmMenu = prompt("Found " + person.firstName + " " + person.lastName + " . What would you like to do?\n1. Get Balance\n2. Withdraw\n3. Deposit\n4. Type your selection or 'restart' or 'quit'");  
+    switch(displayAtmMenu){
+      case "Get Balance":
+        atmInfo.getBalance();
+        break;
+      case "Withraw":
+        atmInfo.withDraw();
+        break;
+      case "Deposit":
+        atmInfo.deposit();
+        break;
+      case "restart":
+        atmAccess(pinAccess); 
+        break;
+      case "quit":
+        return; 
+        default:
+        return atmAccess(pinAccess);
+    }
+  }
+
+  
+  function atmAccess(pinAccess){
+    let atmSearchType = promptFor("Do you have a pin number? Enter 'yes' or 'no'", yesNo).toLowerCase();
+    let atmSearchResults;
+    switch(atmSearchType){
+      case 'yes':
+        atmSearchResults = atmInfo.validatePin();
+        break;
+      case 'no':
+        default:
+      atmAccess(pinAccess); 
+        break;
+    }
+    
+  }
+  
+  return atmMenu();
